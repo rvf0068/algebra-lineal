e56b426cbcb65a599592893067508ec4d34102ef
Rafael Villarroel <rvf0068@gmail.com>
2013-07-26 12:40:46 -0500

    configuration files

--- config.rb
+++ config.rb
@@ -4,11 +4,11 @@ require 'sass-globbing'
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/"
-http_images_path = "/images"
+http_path = "/algebra-lineal/"
+http_images_path = "/algebra-lineal/images"
 http_generated_images_path = "/images"
-http_fonts_path = "/fonts"
-css_dir = "public/stylesheets"
+http_fonts_path = "/algebra-lineal/fonts"
+css_dir = "public/algebra-lineal/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
