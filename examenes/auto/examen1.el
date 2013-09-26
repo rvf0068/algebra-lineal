d92c8ffc6aeda9e058323bdabeaf289b7accff7f
Rafael Villarroel <rvf0068@gmail.com>
2013-09-25 19:56:07 -0500

    Site updated at 2013-09-26 00:56:07 UTC

---
+++ examenes/auto/examen1.el
@@ -0,0 +1,23 @@
+(TeX-add-style-hook
+ "examen1"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("exam" "12pt")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("babel" "spanish" "mexico") ("inputenc" "latin1") ("fontenc" "T1")))
+   (TeX-run-style-hooks
+    "latex2e"
+    "exam"
+    "exam12"
+    "babel"
+    "inputenc"
+    "fontenc"
+    "fullpage"
+    "amsmath"
+    "amsfonts"
+    "enumerate"
+    "fourier")
+   (TeX-add-symbols
+    '("setof" 2)
+    "RR")))
+
